c3e0d889cbd3684f0c3c744a1cb23e6c72172b0d
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-02-12 21:13:17 +1100

    feat: Props - Pass data from parent to child for trans list

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,17 @@ import AddTransaction from './AddTransaction';
 import TransactionList from './TransactionList';
 
 function App() {
+
+  const transactions =[
+    {id:1, item : "Hamburger", cost:19},
+    {id:2, item : "Medicine", cost:28},
+]
+
   return (
     <div className="ui container">
       <Header/>
       <AddTransaction/>
-      <TransactionList/>
+      <TransactionList transactions = {transactions}/>
 
     </div>
   );
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,8 +1,20 @@
 import React from "react";
 
-const ContactList = () => {
+const TransactionList = (props) => {
+    console.log(props);
 
-  return <div className="ui celled list">Transaction List</div>;
-};
+    const renderTransactionList = props.transactions.map((transaction) => {
+      return (
+        <div className="item">
+        <div className="content">
+          <div className="header">{transaction.item}</div>
+          <div>{transaction.cost}</div>
+        </div>
+        <i className="trash alternate outline icon"></i>
+      </div>
+      );
+    });
+    return <div className="ui celled list">{renderTransactionList}</div>;
+  };
 
-export default ContactList;
+export default TransactionList;
