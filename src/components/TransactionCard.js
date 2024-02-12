a22694b3660678381635b263acc566366acf8d73
Jason Ching Yuen Siu <sjsa3a28@gmail.com>
2024-02-12 21:27:09 +1100

    fe: Add images in item list

--- src/components/TransactionCard.js
+++ src/components/TransactionCard.js
@@ -1,14 +1,17 @@
 import React from "react";
-
+import item_img from "../image/item.png"
 const TransactionCard = (props) => {
   const { id, item, cost } = props.transaction;
   return (
     <div className="item">
+        <img className="ui avatar image" src = {item_img} alt="item"/>
       <div className="content">
         <div className="header">{item}</div>
         <div>{cost}</div>
       </div>
-      <i className="trash alternate outline icon"></i>
+      <i className="trash alternate outline icon"
+       style={{ color: 'red', marginTop: '7px' }}
+       onClick={() => props.clickHandler(id)}></i>
     </div>
   );
 };
